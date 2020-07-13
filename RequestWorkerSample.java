c74b5860c416d305faf67d8ee4937c1452d2bba4
Arnish Gupta <guptaarnish.it@gmail.com>
2020-07-13 22:20:36 +0530

    Delete RequestWorkerSample.java

--- RequestWorkerSample.java
+++
@@ -1,8 +0,0 @@
-public class RequestWorker {
-public void execute() throws InterruptedException{
-  for(int i=0; i<=50; i++){
-   System.out.println("Worker: Number:- " + i);
-   Thread.sleep(200);
-  }
- }
-}
